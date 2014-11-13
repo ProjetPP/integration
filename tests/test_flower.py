7557be97a797ecbb1a5775192b10580aa7614428
Valentin Lorentz <progval@progval.net>
2014-11-13 17:04:31 +0100

    More tests.

--- tests/test_flower.py
+++ tests/test_flower.py
@@ -1,6 +1,13 @@
 import requests
 from unittest import TestCase
 
+from ppp_datamodel.communication import Response
+
 class FlowerTestCase(TestCase):
     def testFlowerAnswers(self):
         r = requests.post('http://ppp.pony.ovh:9000/core/', data='{"id": "", "language": "en", "trace": [], "measures": {}, "tree": {"type": "triple", "subject": {"type": "resource", "value": "you"}, "object": {"type": "missing"}, "predicate": {"type": "resource", "value": "be"}}}').json()
+        r = list(map(Response.from_dict, r))
+        self.assertEqual(len(r), 1, r)
+        o = r[0]
+        self.assertEqual(o.tree.type, 'resource')
+        self.assertIn(o.tree.value, {'bot', 'computer', 'flower'})
