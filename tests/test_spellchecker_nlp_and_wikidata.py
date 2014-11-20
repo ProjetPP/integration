1b0678f59445a7bb8b23aaa5e563acf63d5ce538
Valentin Lorentz <progval@progval.net>
2014-11-20 14:49:51 +0100

    Add test case for SpellChecker->NLP->Wikidata.

---
+++ tests/test_spellchecker_nlp_and_wikidata.py
@@ -0,0 +1,15 @@
+import requests
+from unittest import TestCase
+
+from ppp_datamodel import Triple
+from ppp_datamodel.communication import Response
+
+class SpellCheckerNlpWikidataTestCase(TestCase):
+    def testSpellCheckerNlpWikidata(self):
+        r = requests.post('http://ppp.pony.ovh:9000/core/', data='{"id": "", "language": "en", "trace": [], "measures": {}, "tree": {"type": "sentence", "value": "What is the birth daet of George Washington?"}}').json()
+        r = list(filter(lambda x:not isinstance(x.tree, Triple),
+                        map(Response.from_dict, r)))
+        self.assertGreaterEqual(len(r), 1, r)
+        for o in r:
+            self.assertEqual(o.tree.type, 'resource')
+            self.assertIn(o.tree.value, {'1732-02-22', '22/2/1732'})
