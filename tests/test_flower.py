1a0c1e68518605b00c0b7ea65dd34806cf4c0e94
Valentin Lorentz <progval@progval.net>
2014-11-24 21:49:20 +0100

    Fix test_flower.

--- tests/test_flower.py
+++ tests/test_flower.py
@@ -5,7 +5,7 @@ from ppp_datamodel.communication import Response
 
 class FlowerTestCase(TestCase):
     def testFlowerAnswers(self):
-        r = requests.post('http://ppp.pony.ovh:9000/core/', data='{"id": "", "language": "en", "trace": [], "measures": {}, "tree": {"type": "triple", "subject": {"type": "resource", "value": "you"}, "object": {"type": "missing"}, "predicate": {"type": "resource", "value": "be"}}}').json()
+        r = requests.post('http://ppp.pony.ovh:9000/core/', data='{"id": "", "language": "en", "trace": [], "measures": {}, "tree": {"type": "triple", "subject": {"type": "resource", "value": "you"}, "object": {"type": "missing"}, "predicate": {"type": "resource", "value": "identity"}}}').json()
         r = list(map(Response.from_dict, r))
         self.assertEqual(len(r), 1, r)
         o = r[0]
