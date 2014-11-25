ab5e38072a7824b14cc8bf7794dfc3d62bdcb291
Valentin Lorentz <progval@progval.net>
2014-11-25 13:03:58 +0100

    Fix tests.

--- tests/test_flower.py
+++ tests/test_flower.py
@@ -2,6 +2,7 @@ import requests
 from unittest import TestCase
 
 from ppp_datamodel.communication import Response
+from example_ppp_module import requesthandler
 
 class FlowerTestCase(TestCase):
     def testFlowerAnswers(self):
@@ -10,4 +11,4 @@ class FlowerTestCase(TestCase):
         self.assertEqual(len(r), 1, r)
         o = r[0]
         self.assertEqual(o.tree.type, 'resource')
-        self.assertIn(o.tree.value, {'bot', 'computer', 'flower'})
+        self.assertIn(o.tree.value, requesthandler.YOU_ARE)
