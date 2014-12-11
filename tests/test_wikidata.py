a0437232ab561740d55adafabc43df4df6e96bfb
Tpt <thomaspt@hotmail.fr>
2014-12-11 14:45:09 +0100

    Merge of Wikipedia in Wikidata

--- tests/test_wikidata.py
+++ tests/test_wikidata.py
@@ -5,7 +5,7 @@ from ppp_datamodel import Triple
 from ppp_datamodel.communication import Response
 
 class WikidataTestCase(TestCase):
-    def testWikidataAnswers(self):
+    def testWikidataTriple(self):
         r = requests.post('http://ppp.pony.ovh:9000/core/', data='{"id": "", "language": "en", "trace": [], "measures": {}, "tree": {"type": "triple", "subject": {"type": "resource", "value": "Douglas Adams"}, "object": {"type": "missing"}, "predicate": {"type": "resource", "value": "birth date"}}}').json()
         r = list(filter(lambda x:not isinstance(x.tree, Triple),
                         map(Response.from_dict, r)))
@@ -13,3 +13,14 @@ class WikidataTestCase(TestCase):
         for o in r:
             self.assertEqual(o.tree.type, 'resource')
             self.assertEqual(o.tree.value, '1952-03-11')
+
+    def testWikidataSentence(self):
+        r = requests.post('http://ppp.pony.ovh:9000/core/', data='{"id": "", "language": "en", "trace": [], "measures": {}, "tree": {"type": "sentence", "value": "Douglas Adams"}}').json()
+        r = list(filter(lambda x:not isinstance(x.tree, Triple),
+                        map(Response.from_dict, r)))
+        self.assertGreaterEqual(len(r), 1, r)
+        got_actual_answer = True
+        for o in r:
+            if o.tree.type == 'resource' and o.tree.value_type == 'wikibase-entity' and o.tree.value == 'Douglas Adams':
+                got_actual_answer = True
+        self.assertTrue(got_actual_answer, r)
