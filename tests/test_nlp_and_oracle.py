f4441adbacf12e359fb102aa9db2e659ae764ea7
Valentin Lorentz <progval@progval.net>
2014-12-21 10:49:58 +0100

    Grmbl.

--- tests/test_nlp_and_oracle.py
+++ tests/test_nlp_and_oracle.py
@@ -7,7 +7,7 @@ from ppp_datamodel.communication import Response
 class OracleTestCase(TestCase):
     def testOracleAnswers(self):
         r = requests.post('http://askplatyp.us:9000/core/', data='{"id": "", "language": "en", "trace": [], "measures": {}, "tree": {"type": "sentence", "value": "What is your website?"}}').json()
-        r = list(filter(lambda x:not isinstance(x.tree, Resource),
+        r = list(filter(lambda x:isinstance(x.tree, Resource),
                         map(Response.from_dict, r)))
         self.assertGreaterEqual(len(r), 1, r)
         for o in r:
