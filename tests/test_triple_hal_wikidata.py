f9bcaa4dc4d21f014c08f5429817047dc9f30df0
Valentin Lorentz <progval@progval.net>
2015-01-11 10:59:11 +0100

    Add a test for the HAL module.

---
+++ tests/test_triple_hal_wikidata.py
@@ -0,0 +1,18 @@
+import requests
+from unittest import TestCase
+
+from ppp_datamodel import Triple, Missing, Resource
+from ppp_datamodel.communication import Response
+
+class TripleWikidataTestCase(TestCase):
+    def testTripleWikidata(self):
+        r = requests.post('http://askplatyp.us:9000/core/', data='{"id": "", "language": "en", "trace": [], "measures": {}, "tree": {"type": "sentence", "value": "((The first cycles in an evolving graph, author, ?), birth date, ?)"}}').json()
+        r = list(map(Response.from_dict, r))
+        self.assertGreaterEqual(len(r), 2, r)
+        got_actual_answer = False
+        for o in r:
+            if o.tree.type == 'resource':
+                self.assertEqual(o.tree.value, '1938-01-10')
+                got_actual_answer = True
+        self.assertTrue(got_actual_answer, r)
+
