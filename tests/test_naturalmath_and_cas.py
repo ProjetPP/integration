8978ffb8d3286479cd67a54187746734700612f8
Valentin Lorentz <progval@progval.net>
2015-01-18 22:50:46 +0100

    Fix test.

--- tests/test_naturalmath_and_cas.py
+++ tests/test_naturalmath_and_cas.py
@@ -8,4 +8,5 @@ class NaturalmathAndCasTestCase(TestCase):
     def testNaturalmathAndCas(self):
         r = requests.post('http://localhost:9000/core/', data='{"id": "", "language": "en", "trace": [], "measures": {}, "tree": {"type": "sentence", "value": "derivate cos(x)"}}').json()
         r = [Response.from_dict(x) for x in r]
-        self.assertIn(r'- \sin{\left (x \right )}', {x.tree.value for x in r})
+        self.assertIn(r'- \sin{\left (x \right )}',
+                {x.tree.value for x in r if isinstance(x.tree, Resource)})
