b2dde4d8e90e8ad5d4e7fdd1803c93303d305cf6
Valentin Lorentz <progval@progval.net>
2015-01-18 22:25:33 +0100

    Fix test.

--- tests/test_naturalmath_and_cas.py
+++ tests/test_naturalmath_and_cas.py
@@ -1,10 +1,11 @@
 import requests
 from unittest import TestCase
 
-from ppp_datamodel import Sentence, Resource
+from ppp_datamodel import Sentence, Resource, MathLatexResource
 from ppp_datamodel.communication import Response
 
 class NaturalmathAndCasTestCase(TestCase):
     def testNaturalmathAndCas(self):
-        r = requests.post('http://askplatyp.us:9000/core/', data='{"id": "", "language": "en", "trace": [], "measures": {}, "tree": {"type": "sentence", "value": "derivate cos(x)"}}').json()
-        self.assertIn(Resource('-sin(x)'), (x.tree for x in r))
+        r = requests.post('http://localhost:9000/core/', data='{"id": "", "language": "en", "trace": [], "measures": {}, "tree": {"type": "sentence", "value": "derivate cos(x)"}}').json()
+        r = [Response.from_dict(x) for x in r]
+        self.assertIn(r'- \sin{\left (x \right )}', {x.tree.value for x in r})
