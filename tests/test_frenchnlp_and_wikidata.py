0c9d8b10209129cdd74895659ef177024c2093d1
Valentin Lorentz <progval@progval.net>
2015-01-25 21:27:49 +0100

    Add test for French NLP.

---
+++ tests/test_frenchnlp_and_wikidata.py
@@ -0,0 +1,16 @@
+import requests
+from unittest import TestCase
+
+from ppp_datamodel import Triple
+from ppp_datamodel.communication import Response
+
+class WikidataTestCase(TestCase):
+    def testWikidataAnswers(self):
+        r = requests.post('http://askplatyp.us:9000/core/', data='{"id": "", "language": "fr", "trace": [], "measures": {}, "tree": {"type": "sentence", "value": "Quelle est la date de naissance d’Obama ?"}}').json()
+        r = list(filter(lambda x:not isinstance(x.tree, Triple),
+                        map(Response.from_dict, r)))
+        self.assertGreaterEqual(len(r), 1, r)
+        for o in r:
+            self.assertEqual(o.tree.type, 'resource')
+            self.assertEqual(o.tree.value, '1952-03-11')
+
